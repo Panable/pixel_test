88295783db79d24bdf4fcae1203b17e312b37b9b
Mason Pynt <masonpynt@icloud.com>
2023-09-20 23:37:20 +1000

    test more

--- render.py
+++ render.py
@@ -1,11 +1,24 @@
 from PIL import Image, ImageDraw, ImageFont
+import numpy as np
+from rgbmatrix import RGBMatrix, RGBMatrixOptions
 
+# Initialize matrix
+options = RGBMatrixOptions()
+options.rows = 32
+options.cols = 64
+options.chain_length = 1
+options.parallel = 1
+options.hardware_mapping = 'adafruit-hat'
+matrix = RGBMatrix(options=options)
+
+# Dimensions for rendering
 width, height = 64, 32
+scaling_factor = 40
+adjusted_width, adjusted_height = 64 * scaling_factor, 32 * scaling_factor
 
-# Assuming that the font is located at the given path, adjust if necessary
+# Font configuration
 font_path = "/usr/local/share/fonts/Dina-Regular-01.ttf"
-time_font_size = 14
-
+time_font_size = 14 * scaling_factor
 time_font = ImageFont.truetype(font_path, time_font_size)
 
 def get_text_dimensions(text_string, font):
@@ -19,109 +32,38 @@ def format_time(current_time):
     hour_str, minute_str = current_time
     return f"{hour_str}:{minute_str}"
 
-def create_time_image(current_time):
-    img = Image.new('RGB', (width, height), color=(0, 0, 0))
-    draw = ImageDraw.Draw(img)
+def render_time_on_matrix(current_time):
+    matrix_img = Image.new('RGB', (adjusted_width, adjusted_height), color=(0, 0, 0))
+    draw = ImageDraw.Draw(matrix_img)
 
     time_str = format_time(current_time)
-    time_str_split = time_str.split(":")
-    hour_str, minute_str = time_str_split
+    hour_str, minute_str = time_str.split(":")
 
     hour_width, hour_height = get_text_dimensions(hour_str, time_font)
     minute_width, minute_height = get_text_dimensions(minute_str, time_font)
     total_width = hour_width + minute_width + 1
 
-    colon_center_x = (width - total_width) // 2 + hour_width + 1
+    colon_center_x = (adjusted_width - total_width) // 2 + hour_width + 1
     hour_x = colon_center_x - hour_width - 1
     minute_x = colon_center_x + 1
 
-    draw.text((hour_x, 0), hour_str, font=time_font, fill=(255, 255, 255))
-    draw.text((minute_x, 0), minute_str, font=time_font, fill=(255, 255, 255))
-    colon_center_y = 7
-    colon_spacing = 2
-    draw.rectangle([(colon_center_x, colon_center_y - colon_spacing),
-                    (colon_center_x, colon_center_y - colon_spacing + 1)], fill=(255, 255, 255))
-    draw.rectangle([(colon_center_x, colon_center_y + colon_spacing),
-                    (colon_center_x, colon_center_y + colon_spacing + 1)], fill=(255, 255, 255))
+    draw.text((hour_x, height // 2), hour_str, font=time_font, fill=(255, 255, 255))
+    draw.text((minute_x, height // 2), minute_str, font=time_font, fill=(255, 255, 255))
 
-    return img
-#from PIL import Image, ImageDraw, ImageFont
-#from getweather import get_weather
-#import os
-#from textwrap import wrap
-#import numpy as np
-#from rgbmatrix import RGBMatrix, RGBMatrixOptions
-#TEMP_FONT_SIZE_FACTOR = 1.0 
-#ICON_SCALE_FACTOR = 0.6  
-#TOP_PADDING = 3
-#
-#width, height = 64, 32
-#
-## Assuming that the font is located at the given path, adjust if necessary
-#font_path = "/usr/local/share/fonts/DinaRemaster-Regular-01.ttf"
-#time_font_size = 14
-#temp_font_size = 12  
-#
-#time_font = ImageFont.truetype(font_path, time_font_size)
-#temp_font = ImageFont.truetype(font_path, temp_font_size)
-#
-#def get_text_dimensions(text_string, font):
-#    text_string = str(text_string)
-#    ascent, descent = font.getmetrics()
-#    text_width = font.getmask(text_string).getbbox()[2]
-#    text_height = font.getmask(text_string).getbbox()[3] + descent
-#    return (text_width, text_height)
-#
-#def format_time(current_time):
-#    hour_str, minute_str = current_time
-#    return f"{hour_str}:{minute_str}"
-#
-#def create_time_image(current_time, city='Sydney'):
-#    img = Image.new('RGB', (width, height), color=(0, 0, 0))
-#    draw = ImageDraw.Draw(img)
-#
-#    time_str = format_time(current_time)
-#    hour_str, minute_str = time_str.split(":")
-#    
-#    hour_width, hour_height = get_text_dimensions(hour_str, time_font)
-#    minute_width, minute_height = get_text_dimensions(minute_str, time_font)
-#    total_width = hour_width + minute_width + 1
-#
-#    colon_center_x = (width - total_width) // 2 + hour_width + 1
-#    hour_x = colon_center_x - hour_width - 1
-#    minute_x = colon_center_x + 1
-#
-#    draw.text((hour_x, TOP_PADDING), hour_str, font=time_font, fill=(255, 255, 255))
-#    draw.text((minute_x, TOP_PADDING), minute_str, font=time_font, fill=(255, 255, 255))
-#    colon_center_y = 7
-#    colon_spacing = 2
-#    draw.rectangle([(colon_center_x, colon_center_y - colon_spacing),
-#                    (colon_center_x, colon_center_y - colon_spacing + 1)], fill=(255, 255, 255))
-#    draw.rectangle([(colon_center_x, colon_center_y + colon_spacing),
-#                    (colon_center_x, colon_center_y + colon_spacing + 1)], fill=(255, 255, 255))
-#
-#    weather_image_path, temp_str = get_weather()
-#    temp_str_with_symbol = f"{temp_str}°C"
-#    
-#    # Load and adjust the size of weather icon 
-#    weather_image = Image.open(weather_image_path)
-#    upscale_factor = int(16 * ICON_SCALE_FACTOR)  
-#    weather_image = weather_image.resize((upscale_factor, upscale_factor))
-#    icon_width, icon_height = weather_image.size
-#
-#    start_y = (height - icon_height) // 2 + TOP_PADDING
-#
-#    start_x = (width - icon_width) // 2
-#
-#    # Position the icon
-#    icon_y = start_y
-#    img.paste(weather_image, (start_x, icon_y))
-#
-#    # Position the temperature
-#    temp_x = start_x + icon_width + 1  
-#    temp_y = icon_y + icon_height - temp_font_size
-#    draw.text((temp_x, temp_y), str(temp_str_with_symbol), font=temp_font, fill=(255, 255, 255))
-#
-#    return img
+    # Convert the original image to numpy array
+    image_np = np.array(matrix_img)
+    
+    # Resize numpy array to the matrix's resolution
+    image_np_resized = np.array(Image.fromarray(image_np).resize((width, height), Image.ANTIALIAS))
 
+    # Convert the resized numpy array back to a PIL image
+    final_image = Image.fromarray(image_np_resized)
+    frame_canvas = matrix.CreateFrameCanvas()
+    frame_canvas.SetImage(final_image)
+    # Display on matrix
+    matrix.SwapOnVSync(frame_canvas)
+
+if __name__ == "__main__":
+    current_time = ("12", "34")  # Example time, can be fetched dynamically
+    render_time_on_matrix(current_time)
 
