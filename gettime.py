e5827c4b717cd26f3bafbd904dfbca27a3bbbb69
Mason Pynt <masonpynt@icloud.com>
2023-09-21 14:52:28 +1000

    newtime

--- gettime.py
+++ gettime.py
@@ -1,9 +1,19 @@
-from datetime import datetime
+import requests
 
-def get_current_time():
-    current_time = datetime.now().time()
-    hour = current_time.hour
-    minute = current_time.minute
-
-    return str(hour).zfill(2), str(minute).zfill(2)
+def get_time_from_api(timezone="Australia/Sydney"):
+    """Fetches the current time for the given timezone using the WorldTimeAPI."""
+    
+    response = requests.get(f"http://worldtimeapi.org/api/timezone/{timezone}")
+    
+    if response.status_code != 200:
+        raise Exception("Failed to fetch time from the API")
+    
+    data = response.json()
+    current_datetime = data['datetime']  # This returns time in the format: '2023-09-20T12:34:56.789+10:00'
+    
+    # Extract just the date and time (without milliseconds)
+    date_str, time_str = current_datetime.split('T')
+    time_str = time_str.split('.')[0]
+    
+    return date_str, time_str
 
