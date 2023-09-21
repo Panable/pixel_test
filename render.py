632ab28a28938aff7d753426c89542e870b57dc0
Mason Pynt <masonpynt@icloud.com>
2023-09-21 22:07:12 +1000

    test

--- render.py
+++ render.py
@@ -23,8 +23,6 @@ width, height = 64, 32
 time_font_size = 12
 
 def get_time_from_api(timezone="Australia/Sydney"):
-    """Fetches the current time for the given timezone using the WorldTimeAPI."""
-    
     response = requests.get(f"http://worldtimeapi.org/api/timezone/{timezone}")
     
     if response.status_code != 200:
@@ -33,85 +31,41 @@ def get_time_from_api(timezone="Australia/Sydney"):
     data = response.json()
     current_datetime = data['datetime']  
     
-    # Extract just the date and time (without milliseconds)
-    date_str, time_str = current_datetime.split('T')
-    time_str = time_str.split('.')[0]
-    
-    hour, minute = time_str.split(":")[:2]
+    hour, minute = current_datetime.split('T')[1].split('.')[0].split(":")[:2]
     return hour, minute
 
 def render_time_and_weather_on_matrix():
     with WandImage(width=width, height=height, background=Color('black')) as matrix_img:
         with Drawing() as draw:
-#            draw.font = 'DinaRemaster-Regular'  # Replace this with the exact name from ImageMagick if different
-#            draw.font_size = time_font_size
-#            
-#            # Display the time
-#            hour, minute = get_time_from_api()
-#            full_time = f"{hour}:{minute}"
-#            text_metrics = draw.get_font_metrics(matrix_img, full_time)
-#            text_width = text_metrics.text_width
-#            x_position_time = (width - text_width) / 2
-#            y_position_time = 2  # Small offset from the top
-#            draw.text(int(x_position_time), int(y_position_time + text_metrics.text_height), full_time)
-            
-            matrix_img = Image(width=width, height=height, background=Color('black'))
-            draw = Drawing()
-            draw.font = "DinaRemaster-Regular-01"  # We're trying without the path to check if Wand recognizes the font.
-            draw.font_size = 12
+            # Font and style configuration
+            draw.font = "DinaRemaster-Regular-01"
+            draw.font_size = time_font_size
             draw.fill_color = Color('white')
             
+            # Fetch and format the current time
             hour, minute = get_time_from_api()
             full_time = f"{hour}:{minute}"
+            
+            # Calculate positioning for center alignment
             text_width = int(draw.get_font_metrics(matrix_img, full_time).text_width)
-              
             x_position_time = (width - text_width) // 2
             y_position_time = 2  # Small offset from the top
-             
+            
+            # Render the time text onto the image
             draw.text(x_position_time, y_position_time, full_time)
             draw(matrix_img)
-              
-              # Convert Wand image to PIL image for the matrix
+            
+            # Convert Wand image to PIL image for the matrix display
             with io.BytesIO() as output:
-             matrix_img.save(file=output, format="PNG")
-             output.seek(0)
-             pil_image = PILImage.open(output)
-            pil_image = pil_image.convert('RGB')
+                matrix_img.save(file=output, format="PNG")
+                output.seek(0)
+                pil_image = PILImage.open(output).convert('RGB')
               
             frame_canvas = matrix.CreateFrameCanvas()
             frame_canvas.SetImage(pil_image)
             matrix.SwapOnVSync(frame_canvas)
-#            # Fetch and display the weather information
-#            weather_icon_path, temperature = get_weather()
-#            with WandImage(filename=weather_icon_path) as icon:
-#                icon_width = icon.width
-#                icon_height = icon.height
-#
-#                temperature_str = f"{temperature}°C"
-#                temp_text_metrics = draw.get_font_metrics(matrix_img, temperature_str)
-#                temp_text_width = temp_text_metrics.text_width
-#                
-#                gap = 2  # Gap between icon and temperature text
-#                combined_width = icon_width + temp_text_width + gap
-#                
-#                x_position_combined = (width - combined_width) / 2
-#                y_position_icon = (height + y_position_time + text_metrics.text_height - icon_height) / 2
-#                
-#                matrix_img.composite(icon, left=int(x_position_combined), top=int(y_position_icon))
-#                
-#                x_position_temp = x_position_combined + icon_width + gap
-#                y_position_temp = y_position_icon + (icon_height - temp_text_metrics.text_height) / 2
-#                draw.text(int(x_position_temp), int(y_position_temp + temp_text_metrics.text_height), temperature_str)
-#                
-#            draw(matrix_img)
-#            pil_image = matrix_img.make_blob('RGB')
-#            pil_image = PILImage.open(io.BytesIO(pil_image))
-#
-#            frame_canvas = matrix.CreateFrameCanvas()
-#            frame_canvas.SetImage(pil_image)
-#
-# Main loop
 
+# Main loop
 if __name__ == "__main__":
     try:
         while True:
