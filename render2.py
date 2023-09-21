62b7e103b2782cc1ad20a60957051090c1d5c053
Mason Pynt <masonpynt@icloud.com>
2023-09-21 18:55:50 +1000

    test script

--- render2.py
+++ render2.py
@@ -14,43 +14,26 @@ matrix = RGBMatrix(options=options)
 # Always render in 64x32
 width, height = 64, 32
 font_path = "/usr/local/share/fonts/DinaRemaster-Regular-01.ttf"
-time_font_size = 12  # as a test
+time_font_size = 12
 time_font = ImageFont.truetype(font_path, time_font_size)
 
-def get_static_time():
-    """Returns a static time for testing."""
-    hour, minute = "12", "34"
-    return hour, minute
+matrix_img = Image.new('RGB', (width, height), color=(0, 0, 0))
+draw = ImageDraw.Draw(matrix_img)
 
-def get_text_dimensions(text_string, font):
-    ascent, descent = font.getmetrics()
-    text_width = font.getmask(text_string).getbbox()[2]
-    text_height = font.getmask(text_string).getbbox()[3] + descent
-    return (text_width, text_height)
+# Display the time ...
+full_time = "12:34"  # Static time for testing
+text_width, text_height = draw.textsize(full_time, font=time_font)
+x_position_time = (width - text_width) / 2
+y_position_time = (height - text_height) / 2
 
-def render_time_on_matrix():
-    matrix_img = Image.new('RGB', (width, height), color=(0, 0, 0))
-    draw = ImageDraw.Draw(matrix_img)
+draw.text((x_position_time, y_position_time), full_time, font=time_font, fill=(255, 255, 255))
+matrix.SetImage(matrix_img)
 
-    # Display the time ...
-    hour, minute = get_static_time()
-    full_time = f"{hour}:{minute}"
-    text_width, text_height = get_text_dimensions(full_time, time_font)
-    x_position_time = (width - text_width) / 2
-    y_position_time = 2  # Small offset from the top
-    draw.text((x_position_time, y_position_time), full_time, font=time_font, fill=(255, 255, 255))
-
-    # Update the matrix ...
-    matrix.SetImage(matrix_img)
-
-# Main loop ...
-if __name__ == "__main__":
-    try:
-        while True:
-            render_time_on_matrix()
-            time.sleep(60)  # Update every minute.
-    except KeyboardInterrupt:
-        print("Exiting...")
-    finally:
-        matrix.Clear()
+try:
+    while True:
+        time.sleep(10)  # Just keeping the loop running without doing anything
+except KeyboardInterrupt:
+    print("Exiting...")
+finally:
+    matrix.Clear()
 
