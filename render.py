42299a15b47a6bf663de2fc29c9ad9ac5500f01f
Mason Pynt <masonpynt@icloud.com>
2023-09-21 22:11:06 +1000

    turns out wand is useless

--- render.py
+++ render.py
@@ -1,13 +1,8 @@
-from wand.image import Image as WandImage
-from wand.drawing import Drawing
-from wand.font import Font
-from wand.color import Color
+
+from PIL import Image as PILImage, ImageDraw, ImageFont
 from rgbmatrix import RGBMatrix, RGBMatrixOptions
 import time
-from getweather import get_weather
-import requests
-import io
-from PIL import Image as PILImage
+import datetime
 
 # Init matrix
 options = RGBMatrixOptions()
@@ -18,60 +13,36 @@ options.parallel = 1
 options.hardware_mapping = 'adafruit-hat'
 matrix = RGBMatrix(options=options)
 
-# Always render in 64x32
+# Settings
 width, height = 64, 32
-time_font_size = 12
+font_path = "/usr/local/share/fonts/DinaRemaster-Regular-01.ttf"  # Adjust path as needed
+font_size = 12
+font = ImageFont.truetype(font_path, font_size)
 
-def get_time_from_api(timezone="Australia/Sydney"):
-    response = requests.get(f"http://worldtimeapi.org/api/timezone/{timezone}")
+def render_time_on_matrix():
+    current_time = datetime.datetime.now().strftime('%H:%M')
     
-    if response.status_code != 200:
-        raise Exception("Failed to fetch time from the API")
+    # Create a new black image
+    pil_image = PILImage.new('RGB', (width, height), 'black')
+    draw = ImageDraw.Draw(pil_image)
     
-    data = response.json()
-    current_datetime = data['datetime']  
+    # Calculate the position to center the text
+    text_width, text_height = draw.textsize(current_time, font=font)
+    x_position = (width - text_width) / 2
+    y_position = (height - text_height) / 2
     
-    hour, minute = current_datetime.split('T')[1].split('.')[0].split(":")[:2]
-    return hour, minute
-
-def render_time_and_weather_on_matrix():
-    with WandImage(width=width, height=height, background=Color('black')) as matrix_img:
-        with Drawing() as draw:
-            # Font and style configuration
-            draw.font = "DinaRemaster-Regular-01"
-            draw.font_size = time_font_size
-            draw.fill_color = Color('white')
-            
-            # Fetch and format the current time
-            hour, minute = get_time_from_api()
-            full_time = f"{hour}:{minute}"
-            
-            # Calculate positioning for center alignment
-            text_width = int(draw.get_font_metrics(matrix_img, full_time).text_width)
-            x_position_time = (width - text_width) // 2
-            y_position_time = 2  # Small offset from the top
-            
-            # Render the time text onto the image
-            draw.text(x_position_time, y_position_time, full_time)
-            draw(matrix_img)
-            
-            # Convert Wand image to PIL image for the matrix display
-            
-            with io.BytesIO() as output:
-                matrix_img.format = 'PNG'
-                matrix_img.save(file=output)
-                output.seek(0)
-                pil_image = PILImage.open(output).convert('RGB')
-              
-            frame_canvas = matrix.CreateFrameCanvas()
-            frame_canvas.SetImage(pil_image)
-            matrix.SwapOnVSync(frame_canvas)
+    # Draw the text on the image
+    draw.text((x_position, y_position), current_time, font=font, fill='white')
+    
+    frame_canvas = matrix.CreateFrameCanvas()
+    frame_canvas.SetImage(pil_image)
+    matrix.SwapOnVSync(frame_canvas)
 
 # Main loop
 if __name__ == "__main__":
     try:
         while True:
-            render_time_and_weather_on_matrix()
+            render_time_on_matrix()
             time.sleep(30)  # Update every 30 seconds. Adjust as needed.
     except KeyboardInterrupt:
         print("Exiting...")
