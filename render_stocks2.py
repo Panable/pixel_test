ddee8661db959bc3cd901b1b532b252e5fc9eacb
Mason Pynt <masonpynt@icloud.com>
2023-09-27 23:45:59 +1000

    testing hzeller

--- render_stocks2.py
+++ render_stocks2.py
@@ -1,4 +1,4 @@
-
+from PIL import Image, ImageDraw, ImageFont
 from getstocks import get_stock_data
 from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics
 
@@ -12,21 +12,19 @@ options.hardware_mapping = 'adafruit-hat'
 matrix = RGBMatrix(options=options)
 
 width, height = 64, 32
+max_chart_height = 20
+chart_start_y = height - max_chart_height
 
-# Load the bdf font
-font_path = "/path/to/7x13.bdf"
+
 font = graphics.Font()
-font.LoadFont(font_path)
+font.LoadFont("/usr/local/share/7x13.bdf")
+color = graphics.Color(255, 255, 255)
 
-# Colors and positions
-text_color_white = graphics.Color(255, 255, 255)
-text_color_blue = graphics.Color(0, 0, 255)
-text_color_red = graphics.Color(255, 0, 0)
 
-ticker_x = 2
-ticker_y = 2
 
-def draw_chart_on_canvas(canvas, daily_close_prices, start_y, polygon_color, line_color):
+
+
+def draw_chart_on_matrix(matrix_img, draw, daily_close_prices, start_y, polygon_color, line_color):
     max_price = max(daily_close_prices)
     min_price = min(daily_close_prices)
 
@@ -37,7 +35,7 @@ def draw_chart_on_canvas(canvas, daily_close_prices, start_y, polygon_color, lin
     padding = 0.10  # 10% padding
     padded_min_price = min_price - padding * (max_price - min_price)
     padded_max_price = max_price + padding * (max_price - min_price)
-
+    
     raw_scaled_prices = [
         chart_area_height - int(((price - padded_min_price) / (padded_max_price - padded_min_price)) * chart_area_height)
         for price in daily_close_prices
@@ -46,50 +44,65 @@ def draw_chart_on_canvas(canvas, daily_close_prices, start_y, polygon_color, lin
     scaled_prices = [start_y + price for price in raw_scaled_prices]
     x_interval = chart_area_width / (len(scaled_prices) - 1)
 
+    polygon_points = [(0, chart_end_y)]
+    for i, price in enumerate(scaled_prices):
+        x_pos = i * x_interval
+        polygon_points.append((x_pos, price))
+    polygon_points.append((width-1, chart_end_y))
+
+    draw.polygon(polygon_points, fill=polygon_color)
+
     for i in range(1, len(scaled_prices)):
-        start_point = (int((i-1) * x_interval), int(scaled_prices[i-1]))
-        end_point = (int(i * x_interval), int(scaled_prices[i]))
-        graphics.DrawLine(canvas, start_point[0], start_point[1], end_point[0], end_point[1], line_color)
+        start_point = ((i-1) * x_interval, scaled_prices[i-1])
+        end_point = (i * x_interval, scaled_prices[i])
+        draw.line([start_point, end_point], fill=line_color, width=1)
+
+    print("First polygon point:", polygon_points[0])
+    print("Some polygon y-values:", [p[1] for p in polygon_points[:5]])
+    return draw
 
 def render_stock_on_matrix(ticker='AAPL'):
-    frame_canvas = matrix.CreateFrameCanvas()
+    matrix_img = Image.new('RGB', (width, height), color=(0, 0, 0))
+    draw = ImageDraw.Draw(matrix_img)
+
     stock_data = get_stock_data(ticker)
+
+    # Using hzeller's library to render text
+    offscreen_canvas = matrix.CreateFrameCanvas()
 
     # Render stock ticker
     ticker_str = stock_data['ticker']
-    graphics.DrawText(frame_canvas, font, ticker_x, ticker_y + font.baseline, text_color_white, ticker_str)
+    graphics.DrawText(offscreen_canvas, font, 2, 8, color, ticker_str)
 
     # Render % change right next to the ticker name
     change_percent_str = f"{stock_data['percent_change']:.2f}%"
-    change_percent_x = width - len(change_percent_str) * 7  # Assuming each char in the bdf font is about 7 pixels wide.
-    graphics.DrawText(frame_canvas, font, change_percent_x, ticker_y + font.baseline, text_color_blue if stock_data['dollar_change'] >= 0 else text_color_red, change_percent_str)
+    graphics.DrawText(offscreen_canvas, font, width - 8 * len(change_percent_str) - 2, 8, color, change_percent_str)
 
     # Render current price below the ticker name
     price_str = f"${stock_data['current_price']:.2f}"
-    price_y = ticker_y + 13  # Assuming the height of the bdf font characters is 13 pixels.
-    graphics.DrawText(frame_canvas, font, ticker_x, price_y + font.baseline, text_color_white, price_str)
+    graphics.DrawText(offscreen_canvas, font, 2, 17, color, price_str)
 
     # Render dollar change right next to the current price
     change_dollar_str = f"${stock_data['dollar_change']:.2f}"
-    change_dollar_x = width - len(change_dollar_str) * 7
-    graphics.DrawText(frame_canvas, font, change_dollar_x, price_y + font.baseline, text_color_blue if stock_data['dollar_change'] >= 0 else text_color_red, change_dollar_str)
-    
-    # Calculate start_y for the chart based on where the last text is drawn
-    chart_start_y = price_y + 13 + 2  # Adjust this if needed.
+    graphics.DrawText(offscreen_canvas, font, width - 8 * len(change_dollar_str) - 2, 17, color, change_dollar_str)
 
-    # Color settings based on price change
+    # Draw the stock chart on the matrix image
     if stock_data['dollar_change'] >= 0:
-        polygon_color = text_color_blue
-        line_color = graphics.Color(127, 126, 255)
+        polygon_color = (0, 0, 255)
+        line_color = (127, 126, 255)
     else:
-        polygon_color = text_color_red
-        line_color = graphics.Color(255, 127, 127)
+        polygon_color = (255, 0, 0)
+        line_color = (255, 127, 127)
+    draw_chart_on_matrix(matrix_img, draw, stock_data['daily_close_prices'], chart_start_y, polygon_color, line_color)
 
-    # Draw the stock chart on the frame canvas
-    draw_chart_on_canvas(frame_canvas, stock_data['daily_close_prices'], chart_start_y, polygon_color, line_color)
+    # Combine the chart with the text rendered with hzeller's library
+    matrix_img = matrix_img.convert('RGB')
+    for y in range(height):
+        for x in range(width):
+            pixel = matrix_img.getpixel((x, y))
+            offscreen_canvas.SetPixel(x, y, pixel[0], pixel[1], pixel[2])
 
-    matrix.SwapOnVSync(frame_canvas)
+    matrix.SwapOnVSync(offscreen_canvas)
 
 if __name__ == "__main__":
     render_stock_on_matrix('AAPL')
-
