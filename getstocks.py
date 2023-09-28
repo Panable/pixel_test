df5ce31744b081137de5dd36c6d426320bba226b
Mason Pynt <masonpynt@icloud.com>
2023-09-28 20:21:16 +1000

    datetime

--- getstocks.py
+++ getstocks.py
@@ -5,9 +5,20 @@ API_KEY = "8ff66016a4316135f02f72c9555e5449"
 BASE_URL = "http://api.marketstack.com/v1/eod"
 TICKER_SYMBOL = "AAPL"  # Default ticker symbol
 
+def fetch_current_date_from_worldtimeapi():
+    response = requests.get('http://worldtimeapi.org/api/ip')
+    
+    if response.status_code == 200:
+        data = response.json()
+        datetime_str = data['datetime']  # Format: '2023-09-20T09:45:12.123456+00:00'
+        date_str = datetime_str.split('T')[0]
+        return datetime.date.fromisoformat(date_str)
+    else:
+        raise Exception(f"Failed to fetch time. Status Code: {response.status_code}")
+
 def get_stock_data(symbol=TICKER_SYMBOL):
     # Get today's date and one month ago
-    today = datetime.date.today()
+    today = fetch_current_date_from_worldtimeapi()
     one_month_ago = today - datetime.timedelta(days=30)
 
     print(f"Fetching data from {one_month_ago} to {today}")
