64fab43a4dede034f105aea972a5fcd69552cb46
Mason Pynt <masonpynt@icloud.com>
2023-09-28 23:04:44 +1000

    I hate this

--- render_stocks2.py
+++ render_stocks2.py
@@ -27,8 +27,8 @@ def clamp(value, min_value, max_value):
 
 
 
+
 def draw_chart_on_matrix(matrix_img, draw, daily_close_prices, start_y, polygon_color, line_color):
-    # Find the dimensions of the matrix
     width, height = matrix_img.size
     
     # Normalize the prices
@@ -36,24 +36,32 @@ def draw_chart_on_matrix(matrix_img, draw, daily_close_prices, start_y, polygon_
     max_price = max(daily_close_prices)
     price_range = max_price - min_price
 
-    # The scaling factor to fit the prices in the matrix height
     scale_factor = (height - start_y) / price_range
-
     normalized_prices = [(price - min_price) * scale_factor for price in daily_close_prices]
-
-    # Adjust the normalized prices so that the chart starts from start_y
     adjusted_prices = [height - (price + start_y) for price in normalized_prices]
 
     # Create the polygon points
     num_data_points = len(daily_close_prices)
-    polygon_points = [(x, adjusted_prices[x]) for x in range(num_data_points)] 
-    polygon_points = [(0, height)] + polygon_points + [(num_data_points-1, height)]  # Adding base points for the polygon
+    columns_per_data_point = width / num_data_points
+
+    polygon_points = []
+    for i in range(num_data_points):
+        start_column = int(i * columns_per_data_point)
+        end_column = int((i + 1) * columns_per_data_point)
+        for x in range(start_column, end_column):
+            polygon_points.append((x, adjusted_prices[i]))
+    polygon_points = [(0, height)] + polygon_points + [(width-1, height)]  # Adding base points for the polygon
 
     # Draw the polygon
     draw.polygon(polygon_points, fill=polygon_color)
 
     # Draw the line on top of the polygon
-    line_points = [(x, adjusted_prices[x]) for x in range(num_data_points)]
+    line_points = []
+    for i in range(num_data_points):
+        start_column = int(i * columns_per_data_point)
+        end_column = int((i + 1) * columns_per_data_point)
+        for x in range(start_column, end_column):
+            line_points.append((x, adjusted_prices[i]))
     draw.line(line_points, fill=line_color)
 
     return matrix_img
