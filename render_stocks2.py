ddfa29802a0c06c40f9839f75d76c9ca25815eb5
Mason Pynt <masonpynt@icloud.com>
2023-09-28 20:28:03 +1000

    chart logic

--- render_stocks2.py
+++ render_stocks2.py
@@ -27,30 +27,48 @@ color = graphics.Color(255, 255, 255)
 def draw_chart_on_matrix(matrix_img, draw, daily_close_prices, start_y, polygon_color, line_color):
     max_price = max(daily_close_prices)
     min_price = min(daily_close_prices)
+    price_range = max_price - min_price
 
-    chart_end_y = height - 1 
+    chart_end_y = height - 1
     chart_area_height = chart_end_y - start_y
     chart_area_width = width
 
+    # Calculate the required padding for your price range
     padding = 0.10  # 10% padding
-    padded_min_price = min_price - padding * (max_price - min_price)
-    padded_max_price = max_price + padding * (max_price - min_price)
-    
-    raw_scaled_prices = [
-        chart_area_height - int(((price - padded_min_price) / (padded_max_price - padded_min_price)) * chart_area_height)
+    padded_price_range = price_range + 2 * padding * price_range
+
+    # Calculate the scale factor for the prices
+    scale_factor = chart_area_height / padded_price_range
+
+    # Adjust the starting point based on whether stock went up or down
+    if daily_close_prices[-1] >= daily_close_prices[0]:  # Stock went up
+        adjusted_start_y = chart_end_y - (max_price + padding * price_range) * scale_factor
+    else:  # Stock went down
+        adjusted_start_y = chart_end_y - (min_price - padding * price_range) * scale_factor
+
+    scaled_prices = [
+        adjusted_start_y + (price - min_price + padding * price_range) * scale_factor
         for price in daily_close_prices
     ]
 
-    scaled_prices = [start_y + price for price in raw_scaled_prices]
     x_interval = chart_area_width / (len(scaled_prices) - 1)
 
     polygon_points = [(0, chart_end_y)]
     for i, price in enumerate(scaled_prices):
         x_pos = i * x_interval
         polygon_points.append((x_pos, price))
-    polygon_points.append((width-1, chart_end_y))
+    polygon_points.append((width - 1, chart_end_y))
 
     draw.polygon(polygon_points, fill=polygon_color)
+
+    for i in range(1, len(scaled_prices)):
+        start_point = ((i-1) * x_interval, scaled_prices[i-1])
+        end_point = (i * x_interval, scaled_prices[i])
+        draw.line([start_point, end_point], fill=line_color, width=1)
+
+    print("First polygon point:", polygon_points[0])
+    print("Some polygon y-values:", [p[1] for p in polygon_points[:5]])
+    return draw(polygon_points, fill=polygon_color)
 
     for i in range(1, len(scaled_prices)):
         start_point = ((i-1) * x_interval, scaled_prices[i-1])
