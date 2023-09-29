917af170c9eee05c894b52f6f70180fef8f99b77
Mason Pynt <masonpynt@icloud.com>
2023-09-29 17:49:52 +1000

    guh

--- render_stocks2.py
+++ render_stocks2.py
@@ -40,16 +40,13 @@ def draw_chart_on_matrix(matrix_img, draw, daily_close_prices, start_y, polygon_
     scaled_prices = [start_y - (price - min_price) * scale_factor for price in daily_close_prices]
     x_interval = width / (len(scaled_prices) - 1)
 
+    # Start with the bottom-right-most point
     polygon_points = [(width - 1, start_y)]
-
     for i, price in enumerate(scaled_prices):
-        if i == 0:
-            x_pos = width - 1
-        elif i == len(scaled_prices) - 1:
-            x_pos = 0
-        else:
-            x_pos = width - (i * x_interval)
-    polygon_points.append((x_pos, price))
+        x_pos = width - (i * x_interval)
+        polygon_points.append((x_pos, price))
+    # End with the bottom-left-most point
+    polygon_points.append((0, start_y))
     draw.polygon(polygon_points, fill=polygon_color)
 
     for i in range(1, len(scaled_prices)):
