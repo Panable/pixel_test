44d875958d65229dd356c4a8e126beb1d884cd48
Mason Pynt <masonpynt@icloud.com>
2023-10-01 11:57:19 +1100

    sry

--- getstocks.py
+++ getstocks.py
@@ -7,21 +7,15 @@ def no_op(*args, **kwargs):
     class DummyCache:
         def __getattribute__(self, name):
             return no_op
-
         def __setattr__(self, name, value):
             pass
-
     return DummyCache()
 
 yfinance.utils._TzCache = no_op
 
-
 def get_stock_data(ticker_symbol='AAPL'):
-    # Fetching the stock data
     stock = yf.Ticker(ticker_symbol)
     hist = stock.history(period="5d")
-
-    # If there's no stock data, return a default structure
     if hist.empty:
         return {
             'ticker': ticker_symbol,
@@ -31,10 +25,7 @@ def get_stock_data(ticker_symbol='AAPL'):
             'dollar_change': 0
         }
 
-    # Extracting daily close prices
     daily_close_prices = hist['Close'].tolist()
-
-    # Calculate the current price, percent change, and dollar change
     current_price = daily_close_prices[-1]
     previous_price = daily_close_prices[-2] if len(daily_close_prices) > 1 else daily_close_prices[0]
     dollar_change = current_price - previous_price
