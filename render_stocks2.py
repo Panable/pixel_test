648e5cef891dcb77c79fe80165841f14ade8b5b6
Mason Pynt <masonpynt@icloud.com>
2023-10-01 14:46:01 +1100

    fix

--- render_stocks2.py
+++ render_stocks2.py
@@ -15,7 +15,6 @@ width, height = 64, 32
 max_chart_height = 15
 chart_start_y = height - max_chart_height
 
-
 font = graphics.Font()
 font.LoadFont("/usr/local/share/fonts/5x8.bdf")
 color = graphics.Color(255, 255, 255)
@@ -26,18 +25,19 @@ def draw_chart_on_matrix(matrix_img, draw, daily_close_prices, start_y, polygon_
     min_price = min(daily_close_prices)
     price_range = max_price - min_price
 
+    if price_range == 0:
+        price_range = 0.01  # Setting a small default value
+
     # Check if the price range is too narrow
     if price_range < 0.1 * max_price:
         price_range = 0.1 * max_price  # This will give the chart a minimum height.
 
-    # Calculate the scale factor for the prices
     scale_factor = max_chart_height / price_range
 
     # Resample the data points to fit into the matrix width
     data_points_per_column = len(daily_close_prices) // width
     resampled_prices = [daily_close_prices[i] for i in range(0, len(daily_close_prices), data_points_per_column)]
     
-    # Convert normalized prices to fit within the chart height and flip the direction
     scaled_prices = [start_y - (price - min_price) * scale_factor for price in resampled_prices]
     x_interval = width / (len(scaled_prices) - 1)
 
@@ -47,34 +47,25 @@ def draw_chart_on_matrix(matrix_img, draw, daily_close_prices, start_y, polygon_
         polygon_points.append((x_pos, price))
     polygon_points.append((width - 1, start_y))
 
-    # Draw the filled polygon first
     draw.polygon(polygon_points, fill=polygon_color)
-
-    # Then draw the line on top of the polygon
     for i in range(1, len(scaled_prices)):
         start_point = ((i-1) * x_interval, scaled_prices[i-1])
         end_point = (i * x_interval, scaled_prices[i])
         draw.line([start_point, end_point], fill=line_color, width=1)
 
-    # Print some of the resampled prices for verification
     print("Resampled Prices:", resampled_prices[:10])
-
     return draw
 
-
 def render_stock_on_matrix(ticker='AAPL'):
-    # Create a new PIL image to draw the chart.
     local_chart_start_y = chart_start_y
     matrix_img = Image.new('RGB', (width, height), color=(0, 0, 0))
     draw = ImageDraw.Draw(matrix_img)
 
     stock_data = get_stock_data(ticker)
     
-    # Set default colors (optional)
-    polygon_color = (255, 255, 255)  # Default to white
-    line_color = (127, 127, 127)     # Default to grey
+    polygon_color = (255, 255, 255)
+    line_color = (127, 127, 127)
     
-    # Determine colors based on stock price change
     if stock_data['dollar_change'] >= 0:
         polygon_color = (0, 0, 255)
         line_color = (127, 126, 255)
@@ -82,7 +73,6 @@ def render_stock_on_matrix(ticker='AAPL'):
         polygon_color = (255, 0, 0)
         line_color = (255, 127, 127)
 
-    # Calculate scaled_prices for adjustment
     daily_close_prices = stock_data['daily_close_prices']
     max_price = max(daily_close_prices)
     min_price = min(daily_close_prices)
@@ -91,15 +81,15 @@ def render_stock_on_matrix(ticker='AAPL'):
     padded_price_range = price_range + 2 * padding * price_range
 
     if padded_price_range == 0:
-        padded_price_range = 1
+        padded_price_range = 0.01
 
     chart_end_y = height - 1
     chart_area_height = chart_end_y - local_chart_start_y
     scale_factor = chart_area_height / padded_price_range
 
-    if daily_close_prices[-1] >= daily_close_prices[0]:  # Stock went up
+    if daily_close_prices[-1] >= daily_close_prices[0]:
         adjusted_start_y = chart_end_y - (max_price + padding * price_range) * scale_factor
-    else:  # Stock went down
+    else:
         adjusted_start_y = chart_end_y - (min_price - padding * price_range) * scale_factor
 
     scaled_prices = [
@@ -107,12 +97,12 @@ def render_stock_on_matrix(ticker='AAPL'):
         for price in daily_close_prices
     ]
     
-    # Adjust chart start based on scaled_prices
     first_last_diff = scaled_prices[-1] - scaled_prices[0]
     if stock_data['dollar_change'] < 0 and first_last_diff > 0:
         local_chart_start_y += first_last_diff
 
-    draw_chart_on_matrix(matrix_img, draw, daily_close_prices, local_chart_start_y, polygon_color, line_color)    # Transfer the PIL image to the offscreen_canvas.
+    draw_chart_on_matrix(matrix_img, draw, daily_close_prices, local_chart_start_y, polygon_color, line_color)
+
     offscreen_canvas = matrix.CreateFrameCanvas()
     matrix_img = matrix_img.convert('RGB')
     for y in range(height):
@@ -120,13 +110,11 @@ def render_stock_on_matrix(ticker='AAPL'):
             pixel = matrix_img.getpixel((x, y))
             offscreen_canvas.SetPixel(x, y, pixel[0], pixel[1], pixel[2])
 
-    # Render text on top of the chart directly on the offscreen_canvas.
     ticker_str = stock_data['ticker']
     change_percent_str = f"{stock_data['percent_change']:.2f}%"
     price_str = f"${stock_data['current_price']:.2f}"
     change_dollar_str = f"${stock_data['dollar_change']:.2f}"
 
-    # Define positioning for each text element:
     ticker_x = 2
     ticker_y = 7
     change_percent_x = width - 5 * len(change_percent_str)
@@ -136,23 +124,15 @@ def render_stock_on_matrix(ticker='AAPL'):
     change_dollar_x = width - 5 * len(change_dollar_str)
     change_dollar_y = 17
 
-    
     if stock_data['dollar_change'] >= 0:
-        change_color = graphics.Color(0, 0, 255)  # Green for price up
+        change_color = graphics.Color(0, 0, 255)
     else:
-        change_color = graphics.Color(255, 0, 0)  # Red for price down
+        change_color = graphics.Color(255, 0, 0)
     
-    # Render text on top of the chart directly on the offscreen_canvas.
-    ticker_str = stock_data['ticker']
     graphics.DrawText(offscreen_canvas, font, ticker_x, ticker_y, color, ticker_str)
-    
-    change_percent_str = f"{stock_data['percent_change']:.2f}%"
     graphics.DrawText(offscreen_canvas, font, change_percent_x, change_percent_y, change_color, change_percent_str)
-    
-    price_str = f"${stock_data['current_price']:.2f}"
     graphics.DrawText(offscreen_canvas, font, price_x, price_y, color, price_str)
-    
-    change_dollar_str = f"${stock_data['dollar_change']:.2f}"
     graphics.DrawText(offscreen_canvas, font, change_dollar_x, change_dollar_y, change_color, change_dollar_str)
 
     matrix.SwapOnVSync(offscreen_canvas)
+
--- test.py
+++ test.py
@@ -1,57 +1,30 @@
-import pygame
-from PIL import Image, ImageDraw
 
-# Initialize pygame
-pygame.init()
-
-# Constants
-WIDTH, HEIGHT = 64, 32
-SCALING_FACTOR = 20  # to upscale the 64x32 for visibility
-
-# Initialize the screen and clock
-screen = pygame.display.set_mode((WIDTH * SCALING_FACTOR, HEIGHT * SCALING_FACTOR))
-pygame.display.set_caption('Emulated 64x32 Display with PIL and Pygame')
-clock = pygame.time.Clock()
-
-def draw_on_pil_image(start_y):
-    img = Image.new('RGB', (WIDTH, HEIGHT), color=(255, 255, 255))
-    d = ImageDraw.Draw(img)
+def draw_chart_on_matrix(matrix_img, draw, daily_close_prices, start_y, polygon_color, line_color):
+    # Find the dimensions of the matrix
+    width, height = matrix_img.size
     
-    # Draw a simple horizontal red line
-    d.line((0, start_y, WIDTH, start_y), fill=(255,0,0), width=1)
-    
-    # Draw a simple green triangle (polygon)
-    test_polygon = [(0, start_y), (16, start_y - 8), (32, start_y + 8), (WIDTH, start_y)]
-    d.polygon(test_polygon, fill=(0,255,0), outline=(0,0,0))
-    
-    return img
-start_y_global = 26  # Initially set to the middle of the screen
+    # Normalize the prices
+    min_price = min(daily_close_prices)
+    max_price = max(daily_close_prices)
+    price_range = max_price - min_price
 
-def main():
-    global start_y_global
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    start_y_global -= 1
-                elif event.key == pygame.K_DOWN:
-                    start_y_global += 1
-        
-        pil_image = draw_on_pil_image(start_y_global)
-        pygame_image = pygame.image.fromstring(pil_image.tobytes(), pil_image.size, pil_image.mode)
-        
-        # Scale for display
-        pygame_image = pygame.transform.scale(pygame_image, (WIDTH * SCALING_FACTOR, HEIGHT * SCALING_FACTOR))
-        
-        screen.blit(pygame_image, (0, 0))
-        pygame.display.flip()
-        
-        clock.tick(60)
-    
-    pygame.quit()
+    # The scaling factor to fit the prices in the matrix height
+    scale_factor = (height - start_y) / price_range
 
-if __name__ == "__main__":
-    main()
+    normalized_prices = [(price - min_price) * scale_factor for price in daily_close_prices]
+
+    # Adjust the normalized prices so that the chart starts from start_y
+    adjusted_prices = [height - (price + start_y) for price in normalized_prices]
+
+    # Create the polygon points
+    polygon_points = [(x, adjusted_prices[x]) for x in range(width)] 
+    polygon_points = [(0, height)] + polygon_points + [(width-1, height)]  # Adding base points for the polygon
+
+    # Draw the polygon
+    draw.polygon(polygon_points, fill=polygon_color)
+
+    # Draw the line on top of the polygon
+    line_points = [(x, adjusted_prices[x]) for x in range(width)]
+    draw.line(line_points, fill=line_color)
+
+    return matrix_img
