a25691ff0e7bce3f1993ce09cc1fbfeb8d566a32
masonpynt <masonpynt@icloud.com>
2023-10-12 22:38:52 +1100

    debug log

--- render_spotify.py
+++ render_spotify.py
@@ -1,30 +1,27 @@
-import math
-import logging
 import requests
+import logging
+from io import BytesIO
 import time
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image
 from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from io import BytesIO
-#Spotify stuff TODO
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="0bd953803d7c40049e275816cc208579",
-                     client_secret="4442c4bde22048808a3b05a600323164",
-                     redirect_uri="http://localhost:8000",
-                     scope="user-read-currently-playing user-read-playback-state"))
+# Spotify setup (Note: Replace these placeholder values with actual ones.)
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="YOUR_CLIENT_ID",
+                                               client_secret="YOUR_CLIENT_SECRET",
+                                               redirect_uri="YOUR_REDIRECT_URI",
+                                               scope="user-read-currently-playing user-read-playback-state"))
 
+# Retrieve track information
 track_info = sp.current_playback()
-
 track_name = track_info['item']['name']
 artist_name = track_info['item']['artists'][0]['name']
 original_artist_name = artist_name
-album_name = track_info['item']['album']['name']
 album_cover_url = track_info['item']['album']['images'][0]['url']
 
-print(track_name, artist_name, album_name, album_cover_url)
-
-# Matrix setup stuff
+# Matrix setup
 options = RGBMatrixOptions()
 options.rows = 32
 options.cols = 64
@@ -39,136 +36,90 @@ font = graphics.Font()
 font.LoadFont(font_path)
 color = graphics.Color(255, 255, 255)
 
-def calculate_deletion_rate(position, A, B, C):
-    return A * math.cos(B * position) + C
+class WindowCanvas:
+    def __init__(self, delegatee, width, height, offset_x, offset_y):
+        self.delegatee = delegatee
+        self.width = width
+        self.height = height
+        self.offset_x = offset_x
+        self.offset_y = offset_y
+    
+    def set_pixel(self, x, y, r, g, b):
+        if x < 0 or x >= self.width or y < 0 or y >= self.height:
+            return  # Clip to prevent drawing outside the window
+        self.delegatee.SetPixel(x + self.offset_x, y + self.offset_y, r, g, b)
+    
+    def clear(self):
+        for y in range(self.height):
+            for x in range(self.width):
+                self.set_pixel(x, y, 0, 0, 0)
+    
+    def fill(self, r, g, b):
+        for y in range(self.height):
+            for x in range(self.width):
+                self.set_pixel(x, y, r, g, b)
+# Get the album cover image
 def get_album_cover_image(url, target_size=(32, 24)):
     response = requests.get(url)
-    img = Image.open(BytesIO(response.content))
+    img = Image.open(BytesIO(response.content))  # Using BytesIO to handle binary content
     img = img.resize(target_size, Image.LANCZOS)
     return img
-
-def draw_or_scroll_text_step(canvas, font, start_x, y, max_width, text, color, scroll_pos, shift=-1.5):
-    text_width = graphics.DrawText(canvas, font, -9999, -9999, color, text)
-
-    if text_width <= max_width:
-        centered_x = start_x + (max_width - text_width) // 2
-        graphics.DrawText(canvas, font, centered_x, y, color, text)
-        return int(centered_x)
-
-    new_pos = int(scroll_pos + shift)
-    
-    if new_pos <= start_x - text_width:
-        new_pos = 64
-
-    graphics.DrawText(canvas, font, new_pos, y, color, text)
-    return int(new_pos)
-
-def calculate_scroll_duration(text_width, max_width, shift):
-
-    total_distance = max_width + text_width
-    duration = total_distance / abs(shift)
-
-    return duration
-# Initial positions
-scroll_pos_title, scroll_pos_artist, scroll_pos_album = 64, 64, 64
-# Individual shift values for title, artist, and album
-shift_title = -1.5
-shift_artist = -1
-shift_album = -1
-
 album_cover = get_album_cover_image(album_cover_url)
 
-# ...
-log_file = open("log.txt", "a")
+# Create two window canvases: one for the album cover and one for the text
+album_cover_window = WindowCanvas(matrix, 32, 32, 0, 0)
+text_window = WindowCanvas(matrix, 32, 32, 32, 0)
+
+# Main loop
 scroll_pos_artist = 64
-
-boundary = 96  # X coordinate boundary after which letters should start being deleted
 shift_artist = -1  # Shift per frame in pixels
-# Save the original artist name for resetting later
-original_artist_name = artist_name
 
-# Initialize a frame counter
-frame_counter = 0
-deleted_chars = 0
+# Define the start x-position and width of the right-side canvas
+right_canvas_start_x = 32  # Assuming the left-side canvas (album cover) is 32 pixels wide
+right_canvas_width = 32    # Assuming the right-side canvas is also 32 pixels wide
 
-# Initialize a frame counter
-frame_counter = 0
+# Initial position of the artist name
+scroll_pos_artist = right_canvas_start_x
 
-# Initialize a deletion counter
-deletion_counter = 0.0
+logging.basicConfig(filename='scrolling_text.log', level=logging.DEBUG)
 
-# Define a deletion rate (you might adjust this to fine-tune the synchronization)
-deletion_rate = abs(shift_artist) / 1.2
+try:
+    while True:
+        offscreen_canvas = matrix.CreateFrameCanvas()
 
-A = 0.5  # Amplitude
-B = 3.7 
-C = 1    # Vertical shift
-while True:
-    offscreen_canvas = matrix.CreateFrameCanvas()
-    
-    # Fetch the album cover and paste it
-    for y in range(8, 32):
-        for x in range(32):
-            pixel = album_cover.getpixel((x, y - 8))
-            offscreen_canvas.SetPixel(x, y, pixel[0], pixel[1], pixel[2])
+        # Draw the album cover
+        for y in range(8, 32):
+            for x in range(32):
+                pixel = album_cover.getpixel((x, y - 8))
+                offscreen_canvas.SetPixel(x, y, pixel[0], pixel[1], pixel[2])
 
-    # Calculate the width of the artist's name
-    text_artist_width = graphics.DrawText(offscreen_canvas, font, -9999, -9999, color, artist_name)
-    # If the text hasn't reached the boundary
-    if scroll_pos_artist + text_artist_width > boundary:  
-        scroll_pos_artist += shift_artist 
-    else:
-        # If there are characters left in the artist name
-        if len(artist_name) > 1:  # Keep at least one character visible
-            deletion_rate = calculate_deletion_rate(scroll_pos_artist, A, B, C)
-            deletion_counter += deletion_rate
-            # Check if it's time to delete a character
-            if deletion_counter >= 1:
-                artist_name = artist_name[1:]  
-                text_artist_width = graphics.DrawText(offscreen_canvas, font, -9999, -9999, color, artist_name)
-                deletion_counter = 0.0
-        else:  
-            artist_name = original_artist_name  
-            scroll_pos_artist = 64
-    
-    graphics.DrawText(offscreen_canvas, font, scroll_pos_artist, 18, color, artist_name)
-    
-    time.sleep(0.07)
-    matrix.SwapOnVSync(offscreen_canvas) 
-    # Increment frame counter
-    frame_counter += 1
-   # while True:
-#     offscreen_canvas = matrix.CreateFrameCanvas()
-# 
-#     # Fetch the album cover and paste it
-#     for y in range(8, 32):
-#         for x in range(32):
-#             pixel = album_cover.getpixel((x, y - 8))
-#             offscreen_canvas.SetPixel(x, y, pixel[0], pixel[1], pixel[2])
-# 
-#     scroll_pos_title = draw_or_scroll_text_step(offscreen_canvas, font, 0, 8, 64, track_name, color, scroll_pos_title, shift_title)
-#     
-#     # Calculate the width of the artist's name
-#     text_artist_width = graphics.DrawText(offscreen_canvas, font, -9999, -9999, color, artist_name)
-#     
-#     # Log details
-#     log_file.write(f"Artist Name: {artist_name}\n")
-#     log_file.write(f"Scroll Position Artist: {scroll_pos_artist}\n")
-#     log_file.write(f"Text Artist Width: {text_artist_width}\n")
-# 
-#     # If the scroll position plus the width of the artist's name is still greater than the boundary, keep scrolling.
-#     if scroll_pos_artist + text_artist_width > boundary:
-#         scroll_pos_artist -= 1
-#     else:
-#         # If there's still text left to display, remove the first character and adjust the scroll position.
-#         if len(artist_name) > 0:
-#             artist_name = artist_name[1:]
-#         else:
-#             # Reset if there's no text left.
-#             artist_name = original_artist_name
-#             scroll_pos_artist = 64
-# 
-#     graphics.DrawText(offscreen_canvas, font, scroll_pos_artist, 18, color, artist_name)
-#     
-#     time.sleep(0.07)
-#     matrix.SwapOnVSync(offscreen_canvas)
+        # Calculate the width of the artist's name
+        text_artist_width = graphics.DrawText(offscreen_canvas, font, -9999, -9999, color, artist_name)
+        
+        # Scroll the artist's name
+        scroll_pos_artist = max(scroll_pos_artist - 1, right_canvas_start_x - text_artist_width)
+
+        # Ensure text does not scroll beyond the right boundary of the right-side canvas
+        if scroll_pos_artist > right_canvas_start_x + right_canvas_width - text_artist_width:
+            scroll_pos_artist = right_canvas_start_x + right_canvas_width - text_artist_width
+
+        # Draw text only within the right-side canvas
+        graphics.DrawText(offscreen_canvas, font, scroll_pos_artist, 18, color, artist_name)
+        
+        # Logging
+        logging.debug(f"Artist Name: {artist_name}")
+        logging.debug(f"Text Artist Width: {text_artist_width}")
+        logging.debug(f"Scroll Position Artist: {scroll_pos_artist}")
+        logging.debug(f"Right Canvas Start X: {right_canvas_start_x}")
+        logging.debug(f"Right Canvas Width: {right_canvas_width}")
+        logging.debug("----")
+
+        matrix.SwapOnVSync(offscreen_canvas)
+        time.sleep(0.07)
+except KeyboardInterrupt:
+    # Graceful exit on Ctrl+C
+    pass
+finally:
+    # Ensure the matrix is cleared on exit
+    offscreen_canvas.Clear()
+    matrix.SwapOnVSync(offscreen_canvas)
